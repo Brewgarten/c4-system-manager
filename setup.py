8768b43f1a8d41abb177510df874e9b165caaf8c
Martin Kuehnhausen <martin.kuehnhausen@us.ibm.com>
2016-01-10 22:21:36 -0600

    Added pytest and coverage integration (closes #6)

--- setup.py
+++ setup.py
@@ -1,7 +1,12 @@
+import sys
+
 from setuptools import setup, find_packages
 
 import versioneer
 
+
+needs_pytest = {"pytest", "test", "ptr"}.intersection(sys.argv)
+pytest_runner = ["pytest-runner"] if needs_pytest else []
 
 setup(
     name = "c4-systemmanager",
@@ -9,6 +14,8 @@ setup(
     cmdclass = versioneer.get_cmdclass(),
     packages = find_packages(),
     install_requires = ["c4-utils", "c4-messaging"],
+    setup_requires=[] + pytest_runner,
+    tests_require=["pytest", "pytest-cov"],
     author = "IBM",
     author_email = "",
     description = "This library provides the system management framework for project C4",
